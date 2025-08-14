531c427a8bf4534d99705ca488437ca5dac0050d
schincharauli <s.tchintcharauli@gmail.com>
2025-08-14 14:07:30 +0400

    feat: added models in the prisma chema

---
+++ lms/app/dashboard/courses/create/page.tsx
@@ -0,0 +1,35 @@
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+export default function CourseCreationPAge() {
+  return (
+    <>
+      <div className="flex items-center gap-4">
+        <Link
+          href="/admin/courses"
+          className={buttonVariants({ variant: "outline", size: "icon" })}
+        >
+          <ArrowLeft className="size-4" />
+        </Link>
+        <h1 className="text-2xl font-bold">Create Courses</h1>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardHeader>
+            <CardTitle>Basic Information</CardTitle>
+            <CardDescription>
+              Provide Basic Information About the course
+            </CardDescription>
+          </CardHeader>
+        </CardHeader>
+      </Card>
+    </>
+  );
+}
