f18b5996b100ed3c1d3113706d2100a1c7e019d7
schincharauli <s.tchintcharauli@gmail.com>
2025-08-16 20:07:41 +0400

    finalized login part

--- lms/app/page.tsx
+++ lms/app/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { authClient } from "@/lib/auth-client";
+
 import Image from "next/image";
 
 export default function Home() {
+  const { data: session } = authClient.useSession();
+
   return (
     <div>
       <h1 className="text-2xl text-red-500 ">hello world</h1>
       <ThemeToggle />
+      {session ? <p>{session.user.name}</p> : <Button>Log in</Button>}
     </div>
   );
 }
