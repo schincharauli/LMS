29c9b4a4a09ac6538972bd3fb1bc3c73a4e3c38b
schincharauli <s.tchintcharauli@gmail.com>
2025-08-16 20:52:20 +0400

    feat: added logout function

--- lms/app/page.tsx
+++ lms/app/page.tsx
@@ -4,15 +4,36 @@ import { ThemeToggle } from "@/components/ui/theme-toggle";
 import { authClient } from "@/lib/auth-client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const router = useRouter();
   const { data: session } = authClient.useSession();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+          toast.success("signed out succesfuly");
+        },
+      },
+    });
+  }
 
   return (
     <div>
       <h1 className="text-2xl text-red-500 ">hello world</h1>
       <ThemeToggle />
-      {session ? <p>{session.user.name}</p> : <Button>Log in</Button>}
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Sign Out</Button>
+        </div>
+      ) : (
+        <Button>Log in</Button>
+      )}
     </div>
   );
 }
